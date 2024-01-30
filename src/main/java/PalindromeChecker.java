9bc3d4e5d34a14cb8f84aed597740852cab4cbb5
witoAPCS <143124114+witoAPCS@users.noreply.github.com>
2024-01-29 20:37:56 -0800

    Update PalindromeChecker.java

--- src/main/java/PalindromeChecker.java
+++ src/main/java/PalindromeChecker.java
@@ -37,13 +37,17 @@ public void tester()
 }
 public boolean palindrome(String word)
 {
-  //your code here
-  return false;
+  String balls = reverse(word); 
+ if(balls.equals(sWord)) return true;
+  return false;  
+ 
 }
 public String reverse(String str)
 {
-    String sNew = new String();
-    //your code here
+  String sNew = new String();
+  for(int i = str.length(); i > 0; i--){
+    sNew = sNew + str.substring(i-1,i);
+  }
     return sNew;
 }
 }
